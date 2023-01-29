ee957dd1968bc2bda29d56780b3240f446a624a0
Omar Miranda <mr.fumble@gmail.com>
2023-01-29 14:18:49 -0600

    local adjustment

--- elk-setup.sh
+++ elk-setup.sh
@@ -3,11 +3,7 @@
 #----------- limits on mmapfs ----------------------------#
 sudo sysctl -w vm.max_map_count=262144 && \\
 
-#----------- Cloning the repo ----------------------------#
-git clone https://github.com/biometrioearth/elastdocker.b.e.git && \\
-
 #----------- Create the certificates --------------------#
-cd elastdocker.b.e && \\
 make setup && \\
 
 #----------- Run the containers --------------------#
